bfb0fa8e28fe62e1847c4c4743780bf31452ffd9
AdminK <Jacobkylewilliamson@yahoo.com>
2022-02-21 11:20:18 -0500

    Rewrite computerPlay from scratch using nested if/else. Doesn't work when declaring computerSelection and playerSelection outside of function, but works when defining them inside.

--- myscript.js
+++ myscript.js
@@ -5,8 +5,44 @@ function computerPlay() {
     } else if (selectItem === 1) {
         return 'paper'
     } else {
-        return 'scissors';
+        return 'scissors'
     }
 }
 
-console.log("The computer chose: " +computerPlay());
+
+
+
+function playGame(playerSelection, computerSelection) {
+
+    computerSelection = computerPlay();
+    playerSelection = prompt("Type rock, paper, or scissors.").toLowerCase();
+
+
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            return 'The game is a tie! Rock ties rock.'
+        } else if (computerSelection == 'paper') {
+            return 'You lose! Paper beats rock.'
+        } else {
+            return 'You win! Scissors loses to rock.'
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            return 'You win! Paper beats rock!'
+        } else if (computerSelection == 'paper') {
+            return 'The game is a tie! Paper ties paper.'
+        } else {
+            return 'You lose! Scissors beats paper.'
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            return 'You lose! Rock beats scissors.'
+        } else if (computerSelection == 'paper') {
+            return 'You win! Scissors beats paper.'
+        } else {
+            return 'The game is a tie! Scissors ties scissors'
+        }
+    }
+}
+
+
