307e200f18710f21c25126175ab3286fc410ac54
AdminK <Jacobkylewilliamson@yahoo.com>
2022-02-21 12:03:18 -0500

    write game function w/ youtube help for format. Remove promt line and computerSelection line from playGame and add to final game function.

--- myscript.js
+++ myscript.js
@@ -13,36 +13,52 @@ function computerPlay() {
 
 
 function playGame(playerSelection, computerSelection) {
-
-    computerSelection = computerPlay();
-    playerSelection = prompt("Type rock, paper, or scissors.").toLowerCase();
-
-
-    if (playerSelection == 'rock') {
-        if (computerSelection == 'rock') {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
             return 'The game is a tie! Rock ties rock.'
-        } else if (computerSelection == 'paper') {
+        } else if (computerSelection === 'paper') {
+            computerScore++;
             return 'You lose! Paper beats rock.'
         } else {
-            return 'You win! Scissors loses to rock.'
+            playerScore++;
+            return 'You win! Rock beats scissors.'
         }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') {
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            playerScore++;
             return 'You win! Paper beats rock!'
-        } else if (computerSelection == 'paper') {
+        } else if (computerSelection === 'paper') {
             return 'The game is a tie! Paper ties paper.'
         } else {
+            computerScore++;
             return 'You lose! Scissors beats paper.'
         }
-    } else if (playerSelection == 'scissors') {
-        if (computerSelection == 'rock') {
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            computerScore++;
             return 'You lose! Rock beats scissors.'
-        } else if (computerSelection == 'paper') {
+        } else if (computerSelection === 'paper') {
+            playerScore++;
             return 'You win! Scissors beats paper.'
         } else {
             return 'The game is a tie! Scissors ties scissors'
         }
+    } else {
+        return "Error!";
     }
 }
 
+playerScore = parseInt(0);
+computerScore = parseInt(0);
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Pick rock, paper, or scissors').toLowerCase();
+        const computerSelection = computerPlay();
+        console.log(playGame(playerSelection, computerSelection));
+        console.log('Player score = ' + playerScore);
+        console.log('Computer score = ' + computerScore);
+    }
+}
+
+game();
